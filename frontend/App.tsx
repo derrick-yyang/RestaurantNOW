22d3f376c38647b1cc1d3e7003030dfce03df61b
kasaytanapat <6338097821@student.chula.ac.th>
2023-11-25 17:22:15 +0800

    initial page

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -13,6 +13,7 @@ function App(): JSX.Element {
   const cameraRef = useRef(null);
   const [capturedUri, setCapturedUri] = useState<string | null>(null);
   const [viewConfirmedPic, setViewConfirmedPic] = useState(false); // New state for confirmed picture view
+  const [viewNext, setViewNext] = useState(false);
 
   const takePicture = async () => {
     if (cameraRef.current) {
@@ -28,25 +29,53 @@ function App(): JSX.Element {
   };
 
   const confirmPicture = () => {
-      console.log('Picture confirmed:', capturedUri);
-      setViewConfirmedPic(true);
-    };
+    console.log('Picture confirmed:', capturedUri);
+    setViewConfirmedPic(true);
+  };
 
-    const goBackToCamera = () => {
-      setViewConfirmedPic(false);
-    };
+  const goBackToCamera = () => {
+    setViewNext(false);
+    setViewConfirmedPic(false);
+  };
+
+  const goToNextView = () => {
+    setViewNext(true);
+    setViewConfirmedPic(false);
+  };
+
+  const goToBackView= () => {
+    setViewConfirmedPic(true);
+  };
 
    if (viewConfirmedPic && capturedUri) {
       return (
         <SafeAreaView style={styles.container}>
-          <Image source={{ uri: capturedUri }} style={styles.fullImage} />
-          <Text style={styles.confirmationText}>sadaadsad</Text>
+
+          <Text style={styles.infoText}>sadaadsad</Text>
           <TouchableOpacity onPress={goBackToCamera} style={styles.backButton}>
             <Text style={styles.buttonText}>Back</Text>
           </TouchableOpacity>
+          <TouchableOpacity onPress={goToNextView} style={styles.navigationButton}>
+            <Text style={styles.buttonText}>Next</Text>
+          </TouchableOpacity>
+
         </SafeAreaView>
       );
     }
+
+   if (viewNext) {
+     return (
+       <SafeAreaView style={styles.container}>
+         {/* Your next view content goes here */}
+         <Text style={styles.infoText}>This is the next view</Text>
+         {/* Add a button to go back to the confirmed picture view or any other navigation */}
+         <TouchableOpacity onPress={goToBackView} style={styles.backButton}>
+           <Text style={styles.buttonText}>Back</Text>
+         </TouchableOpacity>
+       </SafeAreaView>
+     );
+   }
+
 
   return (
       <SafeAreaView style={styles.container}>
@@ -80,8 +109,6 @@ function App(): JSX.Element {
             </TouchableOpacity>
           </>
         )}
-
-        {/* Modal is removed */}
       </SafeAreaView>
     );
   }
@@ -108,7 +135,6 @@ const styles = StyleSheet.create({
       borderRadius: 50, // This will give you rounded corners
       overflow: 'hidden', // This ensures that the camera view does not bleed outside the border radius
       alignSelf: 'center', // Center the camera view horizontally
-      // Remove justifyContent and alignItems if you want the camera to fill the preview box
     },
   capture: {
       justifyContent: 'center',
@@ -141,69 +167,77 @@ const styles = StyleSheet.create({
   captureText: {
     fontSize: 14,
     color: 'white',
-  },
+    },
   capturedImage: {
       width: '80%', // Adjust as needed
       height: '80%', // Adjust as needed
       borderRadius: 20,
     },
-    centeredView: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        marginTop: 20
-      },
+  centeredView: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      marginTop: 20
+    },
 
-      modalImage: {
-        width: '90%', // Set the width as a percentage of the screen width
-        height: '80%', // Adjust the height as needed
-        borderRadius: 20, // Rounded corners for the image
-      },
-       buttonContainer: {
-           flexDirection: 'row', // places buttons in a row
-           justifyContent: 'center', // centers buttons horizontally
-           alignItems: 'center', // centers buttons vertically
-           alignSelf: 'center', // centers the container itself
-           margin: '7%',
-         },
-         actionButton: {
-           width: 100,
-           height: 40,
-           borderRadius: 20,
-           backgroundColor: 'red', // change as needed
-           justifyContent: 'center',
-           alignItems: 'center',
-           marginHorizontal: '7%', // adds spacing between buttons
-         },
-         buttonText: {
-           fontSize: 16,
-           color: 'white', // change as needed
-         },
-          fullImage: {
-             width: '100%', // Full width
-             height: '50%', // Adjust the height as needed, taking up half the screen
-             resizeMode: 'cover', // or 'contain' to see the whole picture without cropping
-           },
-           confirmationText: {
-             fontSize: 30,
-             textAlign: 'center',
-             color: 'black',
-             marginTop: '40%', // Adjust as needed for your layout
-           },
-    backButton: {
-        position: 'absolute',
-        width: 100,
-        top: '90%', // You can adjust this value as needed
-        right: 150, // You can adjust this value as needed
-        backgroundColor: 'red', // Or any other visible color
-        padding: 10,
-        borderRadius: 5,
-      },
-      buttonText: {
-        fontSize: 16,
-        color: 'white', // Ensure this is visible against the backButton color
-        textAlign: 'center',
-      },
+  modalImage: {
+      width: '90%', // Set the width as a percentage of the screen width
+      height: '80%', // Adjust the height as needed
+      borderRadius: 20, // Rounded corners for the image
+     },
+  buttonContainer: {
+      flexDirection: 'row', // places buttons in a row
+      justifyContent: 'center', // centers buttons horizontally
+      alignItems: 'center', // centers buttons vertically
+      alignSelf: 'center', // centers the container itself
+      margin: '7%',
+     },
+  actionButton: {
+      width: 100,
+      height: 40,
+      borderRadius: 20,
+      backgroundColor: 'red', // change as needed
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginHorizontal: '7%', // adds spacing between buttons
+     },
+  buttonText: {
+      fontSize: 16,
+      color: 'white',
+      textAlign: 'center',
+     },
+  displayImage: {
+      width: '100%', // Full width
+      height: '30%', // Adjust the height as needed, taking up half the screen
+      resizeMode: 'cover', // or 'contain' to see the whole picture without cropping
+     },
+  infoText: {
+      fontSize: 30,
+      textAlign: 'center',
+      color: 'black',
+      marginTop: '40%', // Adjust as needed for your layout
+     },
+  backButton: {
+      position: 'absolute',
+      width: 100,
+      top: '90%', // You can adjust this value as needed
+      right: 230, // You can adjust this value as needed
+      backgroundColor: 'red', // Or any other visible color
+      padding: 10,
+      borderRadius: 20,
+     },
+
+ // Adjust the style for the new "Next" button
+   navigationButton: {
+     position: 'absolute',
+     width: 100,
+     top: '90%', // You can adjust this value as needed
+     right: 70, // You can adjust this value as needed
+     backgroundColor: 'red', // Or any other visible color
+     padding: 10,
+     borderRadius: 20,
+     // other styles as needed
+   },
 });
 
 export default App;
